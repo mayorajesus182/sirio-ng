670d9f21f282cf628da85d7b77d3faa6da55d009
Mario Castro <mcastrob.ve@gmail.com>
2022-09-19 22:24:20 -0400

    impl toolbar lang

--- src/@sirio/shared/click-outside/click-outside.directive.ts
+++ src/@sirio/shared/click-outside/click-outside.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
-  selector: '[furyClickOutside]'
+  selector: '[sirioClickOutside]'
 })
 export class ClickOutsideDirective {
 
@@ -9,7 +9,7 @@ export class ClickOutsideDirective {
   }
 
   @Output()
-  public furyClickOutside = new EventEmitter<MouseEvent>();
+  public sirioClickOutside = new EventEmitter<MouseEvent>();
 
   @HostListener('document:click', ['$event', '$event.target'])
   public onClick(event: MouseEvent, targetElement: HTMLElement): void {
@@ -19,7 +19,7 @@ export class ClickOutsideDirective {
 
     const clickedInside = this._elementRef.nativeElement.contains(targetElement);
     if (!clickedInside) {
-      this.furyClickOutside.emit(event);
+      this.sirioClickOutside.emit(event);
     }
   }
 
