12f16c22fdbe44ea94b796e728bc40d760d477a4
Mario Castro <mcastrob.ve@gmail.com>
2022-09-27 15:42:29 -0400

    mejoras visuales, tablas ejemplos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,11 +21,16 @@ const routes: Routes = [
     path: 'components',
     loadChildren: () => import('./pages/components/components.module').then(m => m.ComponentsModule),
   },
+  
   {
     path: 'sirio',
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
+      {
+        path: 'help',
+        loadChildren: () => import('./pages/help-components/components.module').then(m => m.HelpComponentsModule),
+      },
       {
         path: 'welcome',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
