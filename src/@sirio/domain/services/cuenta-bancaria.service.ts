d65522a75430f8243d4c11e88698e4caeea00cfc
Mario Castro <mcastrob.ve@gmail.com>
2022-10-10 15:39:16 -0400

    fix bug

--- src/@sirio/domain/services/cuenta-bancaria.service.ts
+++ src/@sirio/domain/services/cuenta-bancaria.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpParams } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { ApiOption, ApiService } from 'src/@sirio/services/api';
 import { ApiConfConstants } from 'src/@sirio/constants';
+import { ApiOption, ApiService } from 'src/@sirio/services/api';
 
 
 export interface CuentaBancariaOperacion {
