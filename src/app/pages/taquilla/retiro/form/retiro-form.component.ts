cb761b1f289abb675a164350cae4a87ac293d0ad
Karen Rivas <krivas@gmail.com>
2022-10-13 16:42:57 -0400

    retiro

--- src/app/pages/taquilla/retiro/form/retiro-form.component.ts
+++ src/app/pages/taquilla/retiro/form/retiro-form.component.ts
@@ -2,10 +2,13 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Injector, OnInit
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { id } from '@swimlane/ngx-datatable';
+import { CalendarDayViewComponent, CalendarEventAction } from 'angular-calendar';
 import { BehaviorSubject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { fadeInRightAnimation } from 'src/@sirio/animations/fade-in-right.animation';
 import { fadeInUpAnimation } from 'src/@sirio/animations/fade-in-up.animation';
+import { GlobalConstants, RegularExpConstants } from 'src/@sirio/constants';
+import { CalendarioService } from 'src/@sirio/domain/services/calendario/calendar.service';
 import { Moneda } from 'src/@sirio/domain/services/configuracion/divisa/moneda.service';
 import { TipoProducto, TipoProductoService } from 'src/@sirio/domain/services/configuracion/producto/tipo-producto.service';
 
@@ -15,6 +18,8 @@ import { Agencia } from 'src/@sirio/domain/services/organizacion/agencia.service
 import { Persona, PersonaService } from 'src/@sirio/domain/services/persona/persona.service';
 import { Retiro, RetiroService } from 'src/@sirio/domain/services/taquilla/retiro.service';
 import { FormBaseComponent } from 'src/@sirio/shared/base/form-base.component';
+
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-retiro-form',
@@ -28,13 +33,19 @@ export class RetiroFormComponent extends FormBaseComponent implements OnInit {
 
     retiro: Retiro = {} as Retiro;
     tipoDocumentos = new BehaviorSubject<TipoDocumento[]>([]); //lista  
-    cuentaBancariaOperacion: CuentaBancariaOperacion = {} as CuentaBancariaOperacion; // un solo registro
+    cuentaBancariaOperacion: CuentaBancariaOperacion = {} as CuentaBancariaOperacion;
+    public tiposDocumentos = new BehaviorSubject<TipoDocumento[]>([]);
+
+    public tipoDocumentosactivos = new BehaviorSubject<TipoDocumento[]>([]);
+
     persona: Persona = {} as Persona;
     agencia: Agencia = {} as Agencia;
     moneda: Moneda = {} as Moneda;
     tipoProductos: TipoProducto = {} as TipoProducto;
     esPagoCheque: boolean = false;
-    
+    esAhorroLibreta: boolean = false;
+    todayValue: moment.Moment;
+
     constructor(
         injector: Injector,
         private fb: FormBuilder,
@@ -42,24 +53,28 @@ export class RetiroFormComponent extends FormBaseComponent implements OnInit {
         private retiroService: RetiroService,
         private cuentaBancariaService: CuentaBancariaService,
         private tipoDocumentoService: TipoDocumentoService,
-        private personaService: PersonaService,
-        private tipoProductoService: TipoProductoService,
+        private calendarioService: CalendarioService,
         private cdr: ChangeDetectorRef) {
         super(undefined, injector);
     }
 
- 
+
 
     ngOnInit() {
-        this.isNew=true;
+        this.isNew = true;
         this.buildForm(this.retiro);
         this.loadingDataForm.next(false);
+
         //trae servicio de TIPO DE DOCUMENTOS   
-        this.tipoDocumentoService.actives().subscribe(data => {
+        this.tipoDocumentoService.activesByTipoPersona(GlobalConstants.PERSONA_NATURAL).subscribe(data => {
             this.tipoDocumentos.next(data);
         });
 
-            
+        this.tipoDocumentoService.actives().subscribe(data => {
+            this.tipoDocumentosactivos.next(data);
+        });
+
+
         // manejo de escritura en el campo NUMERO DE CUENTA
         this.f.numeroCuenta.valueChanges.pipe(
             distinctUntilChanged(),
@@ -69,10 +84,10 @@ export class RetiroFormComponent extends FormBaseComponent implements OnInit {
             const numeroCuenta = this.f.numeroCuenta.value;
 
             if (numeroCuenta) {
-                this.cuentaBancariaService.activesByNumeroCuenta(numeroCuenta).subscribe(data => {                   
-                    this.cuentaBancariaOperacion = data;  
-                   //const cuentaBancaria = data.id;
-                   // console.log("DATOS", data);                   
+                this.cuentaBancariaService.activesByNumeroCuenta(numeroCuenta).subscribe(data => {
+                    this.cuentaBancariaOperacion = data;
+                    //const cuentaBancaria = data.id;
+                    // console.log("DATOS", data);                   
                     this.cdr.markForCheck();
 
                 }, err => {
@@ -85,43 +100,45 @@ export class RetiroFormComponent extends FormBaseComponent implements OnInit {
         });
 
 
-        this.loading$.subscribe(val=>{
-            if(val){
-                this.persona= {} as Persona;
+        this.loading$.subscribe(val => {
+            if (val) {
+                this.persona = {} as Persona;
                 this.cuentaBancariaOperacion = {} as CuentaBancariaOperacion;
-               
+
             }
+        });
+
+        this.calendarioService.today().subscribe(data => {
+            this.todayValue = moment(data.today, GlobalConstants.DATE_SHORT);
+            this.cdr.detectChanges();
+          });
+    }
+
+    buildForm(retiro: Retiro) {
+
+        this.itemForm = this.fb.group({
+
+            esPagoCheque: new FormControl(false),
+
+            numper: new FormControl(retiro.numper || undefined),
+            tipoDocumentoBeneficiario: new FormControl(retiro.tipoDocumentoBeneficiario || undefined, [Validators.required]),
+            identificacionBeneficiario: new FormControl(this.retiro.identificacionBeneficiario || '', [Validators.pattern(RegularExpConstants.NUMERIC)]),
+            monto: new FormControl(retiro.monto || '', [Validators.required]),
+            numeroCuenta: new FormControl(retiro.numeroCuenta || '', [Validators.required]),
+            moneda: new FormControl([retiro.moneda || '']),
+            tipoProducto: new FormControl([retiro.tipoProducto || '']),
+            serialCheque: new FormControl(retiro.serialCheque || undefined, [Validators.required, Validators.pattern(RegularExpConstants.NUMERIC)]),
+            montocheque: new FormControl(retiro.montocheque || '', [Validators.required,]),
+            fechaEmision: new FormControl(retiro.fechaEmision || '', [Validators.required,]),
+            codSeguridad: new FormControl(retiro.codSeguridad || '', [Validators.pattern(RegularExpConstants.NUMERIC)]),
+            email: new FormControl(retiro.email || ''),
+            telefono: new FormControl(retiro.telefono || ''),
+
+
         });
 
 
 
-    }
-
-    buildForm(retiro: Retiro) {    
-      
-        this.itemForm = this.fb.group({ 
-
-            esPagoCheque: new FormControl(false),        
-                 
-            numper: new FormControl(retiro.numper || undefined),
-          // cuentaBancaria: new FormControl([retiro.cuentaBancaria || '', [Validators.required, ]]),
-          tipoDocumentoBeneficiario: new FormControl(retiro.tipoDocumentoBeneficiario || '', [Validators.required]),
-            identificacionBeneficiario: new FormControl(this.retiro.identificacionBeneficiario || '', [Validators.required]),
-            //nombre: new FormControl(retiro.nombre || '', [Validators.required, Validators.pattern(RegularExpConstants.ALPHA_NUMERIC)]),
-            monto: new FormControl(retiro.monto || '', [Validators.required]),
-            numeroCuenta: new FormControl(retiro.numeroCuenta || '', [Validators.required]),
-            moneda: new FormControl([retiro.moneda || '']),
-            tipoProducto: new FormControl([retiro.tipoProducto || '', [Validators.required,]]),
-           // estatusOperacion: new FormControl([retiro.estatusOperacion || '', [Validators.required, Validators.pattern(RegularExpConstants.ALPHA_NUMERIC)]]),
-           serialCheque: new FormControl(retiro.serialCheque || '', [Validators.required,]),
-           montocheque: new FormControl(retiro.montocheque || '', [Validators.required,]),  
-           fechaEmision: new FormControl(retiro.fechaEmision || '', [Validators.required,]),
-           codSeguridad: new FormControl(retiro.codSeguridad || '', [Validators.required,]),
-           email: new FormControl(retiro.email || ''  ),    
-           telefono: new FormControl(retiro.telefono || ''),    
-            
-        });    
-       
 
     }
 
@@ -129,25 +146,25 @@ export class RetiroFormComponent extends FormBaseComponent implements OnInit {
 
 
     save() {
-       
-        
+
+
         if (this.itemForm.invalid)
             return;
-                       
+
 
         this.updateData(this.retiro);
-        this.updateDataFromValues (this.retiro,this.persona);
+        this.updateDataFromValues(this.retiro, this.persona);
 
-        this.updateDataFromValues (this.retiro,this.cuentaBancariaOperacion);
-        this.retiro.cuentaBancaria = this.cuentaBancariaOperacion.id;      
+        this.updateDataFromValues(this.retiro, this.cuentaBancariaOperacion);
+        this.retiro.cuentaBancaria = this.cuentaBancariaOperacion.id;
         this.retiro.tipoDocumento = this.cuentaBancariaOperacion.tipoDocumento;
         this.retiro.fechaEmision = this.retiro.fechaEmision.format('DD/MM/YYYY');
-      
-       console.log("DATOSS3   ", this.retiro);
-       
-       this.saveOrUpdate(this.retiroService, this.retiro, 'el pago del cheque');
 
-       
+        console.log("DATOSS3   ", this.retiro);
+
+        this.saveOrUpdate(this.retiroService, this.retiro, 'el pago del cheque');
+
+
 
     }
 }
