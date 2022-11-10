377e2f447067cd1f043aa7d0b9e74b1f55a77fa6
Mario Castro <mcastrob.ve@gmail.com>
2022-11-10 13:21:27 -0400

    impl redirect page by rol user

--- src/app/layout/toolbar/toolbar-quickpanel-toggle/toolbar-quickpanel-toggle.component.ts
+++ src/app/layout/toolbar/toolbar-quickpanel-toggle/toolbar-quickpanel-toggle.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { SessionService } from 'src/@sirio/services/session.service';
 
 @Component({
   selector: 'sirio-toolbar-quickpanel-toggle',
@@ -10,11 +11,21 @@ export class ToolbarQuickpanelToggleComponent {
 
   @Output() openQuickPanel = new EventEmitter();
 
-  constructor(private router:Router) {
+  constructor(private router:Router, private sessionService:SessionService) {
   }
 
   goStatistics(){
     //TODO: ACA DEBO PEDIR EL ROL  Y VER PARA DONDE ME VOY SI AGENCIA O TAQUILLA
-    this.router.navigate(['/sirio/estadistica/agencia/saldos']);
+
+    const user = this.sessionService.getUser();
+
+    if (user && user.username && user.rols && user.rols.length > 0) {
+      console.log("Usuario", user);
+      if(user.rols.include("")){
+        this.router.navigate(['/sirio/estadistica/agencia/saldos']);        
+      }else if(user.rols.include("")){
+        this.router.navigate(['/sirio/estadistica/taquilla/saldos']);
+      }
+    }
   }
 }
