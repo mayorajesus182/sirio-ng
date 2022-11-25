17bded1a4186b8cd33e2827544b9b8434df097cb
Octavio Dimas <odimas77@gmail.com>
2022-11-25 12:38:16 -0400

    solicitud de remesa

--- src/app/pages/control-efectivo/remesa/despachar-remesa/form/despachar-remesa-form.component.ts
+++ src/app/pages/control-efectivo/remesa/despachar-remesa/form/despachar-remesa-form.component.ts
@@ -9,7 +9,9 @@ import { GlobalConstants } from 'src/@sirio/constants';
 import { MaterialRemesa, Remesa, RemesaService } from 'src/@sirio/domain/services/control-efectivo/remesa.service';
 import { Preferencia, PreferenciaService } from 'src/@sirio/domain/services/preferencias/preferencia.service';
 import { EmpleadoTransporte, EmpleadoTransporteService } from 'src/@sirio/domain/services/transporte/empleados/empleado-transporte.service';
-import { TransportistaService } from 'src/@sirio/domain/services/transporte/transportista.service';
+import { Transportista, TransportistaService } from 'src/@sirio/domain/services/transporte/transportista.service';
+import { Viaje } from 'src/@sirio/domain/services/transporte/viaje.service';
+import { ViajeTransporteService } from 'src/@sirio/domain/services/transporte/viajes/viaje-transporte.service';
 import { Rol, RolService } from 'src/@sirio/domain/services/workflow/rol.service';
 import { FormBaseComponent } from 'src/@sirio/shared/base/form-base.component';
 import swal, { SweetAlertOptions } from 'sweetalert2';
@@ -28,6 +30,8 @@ export class DespacharRemesaFormComponent extends FormBaseComponent implements O
     private opt_swal: SweetAlertOptions;
     remesa: Remesa = {} as Remesa;
     public empleados = new BehaviorSubject<EmpleadoTransporte[]>([]);
+    public transportistas = new BehaviorSubject<Transportista[]>([]);
+    public viajes = new BehaviorSubject<Viaje[]>([]);
     rol: Rol = {} as Rol;
     preferencia: Preferencia = {} as Preferencia;
     workflow: string = undefined;
@@ -45,6 +49,7 @@ export class DespacharRemesaFormComponent extends FormBaseComponent implements O
         private remesaService: RemesaService,
         private preferenciaService: PreferenciaService,
         private empleadoTransporteService: EmpleadoTransporteService,
+        private viajeTransporteService: ViajeTransporteService,
         private transportistaService: TransportistaService,
         private cdr: ChangeDetectorRef) {
         super(undefined, injector);
@@ -62,25 +67,23 @@ export class DespacharRemesaFormComponent extends FormBaseComponent implements O
             this.rolService.getByUsuario().subscribe(rol => {
                 this.esTransportista = (rol.id === GlobalConstants.TRANSPORTISTA);
 
+                this.rolService.getByUsuario().subscribe(rol => {
+                    this.esTransportista = (rol.id === GlobalConstants.TRANSPORTISTA);
 
-
-
-
-
-
-
-                
+                    // Si quien va a procesar la solicitud es transportista (Centro de Acopio), se buscan los viajes de esa transportista según la moneda 
+                    if (this.esTransportista) {
+                        this.loadViajesAndEmpleados(this.remesa.receptor);
+                    } else {
+                        this.transportistaService.actives().subscribe(trans => {
+                            this.transportistas.next(trans);
+                            this.loadViajesAndEmpleados(this.remesa.transportista);
+                        });
+                    }
+                });
             });
-
-
-
 
             this.preferenciaService.get().subscribe(pref => {
                 this.preferencia = pref;
-            });
-
-            this.empleadoTransporteService.allByTransportista(this.remesa.receptor).subscribe(emp => {
-                this.empleados.next(emp);
             });
 
             this.cdr.markForCheck();
@@ -95,9 +98,40 @@ export class DespacharRemesaFormComponent extends FormBaseComponent implements O
         this.opt_swal.preConfirm = this.preConfirmFunt;
     }
 
+    loadViajesAndEmpleados(value: string) {
+
+        this.empleadoTransporteService.allByTransportista(value).subscribe(emp => {
+            this.empleados.next(emp);
+        });
+
+        this.preferenciaService.get().subscribe(pref => {
+            this.preferencia = pref;
+            // Si es moneda local se bucan los viajes y materiales con bolivares mayores a cero, de otro modo se buscan viajes y materiales con divisas meyores a cero
+            if (this.preferencia.monedaConoActual === this.remesa.moneda) {
+
+                this.viajeTransporteService.allWithCostoByTransportista(value).subscribe(vjt => {
+                    this.viajes.next(vjt);
+                });
+
+            } else {
+
+                this.viajeTransporteService.allWithCostoDivisaByTransportista(value).subscribe(vjt => {
+                    this.viajes.next(vjt);
+                });
+            }
+        });
+    }
+
     buildForm(remesa: Remesa) {
         this.itemForm = this.fb.group({
+            transportista: new FormControl(remesa.transportista || undefined),
+            viaje: new FormControl(remesa.viaje || undefined, [Validators.required]),
             responsables: new FormControl(remesa.responsables || undefined, [Validators.required]),
+        });
+
+        // Sólo se escoge la transportista cuando quien procesa es el centro de acopio, es decir, esto sólo pasará si la pantalla se muestra al centro de acopio
+        this.f.transportista.valueChanges.subscribe(value => {
+            this.loadViajesAndEmpleados(value);
         });
     }
 
@@ -113,7 +147,6 @@ export class DespacharRemesaFormComponent extends FormBaseComponent implements O
             );
         }
     }
-
 
     save() {
         if (this.itemForm.invalid)
