1f87aa99c5f150f91b8650c0d1201328f3148b6e
Mario Castro <mcastrob.ve@gmail.com>
2022-12-21 20:56:21 -0400

    impl estadisticas principal

--- src/@sirio/constants/rol.constants.ts
+++ src/@sirio/constants/rol.constants.ts
@@ -2,6 +2,7 @@ export class RolConstants {
   public static OPERADOR_TAQUILLA = 'OPERAD-TAQ';
   public static GERENTE_TESORERO_AGENCIA = 'GER-TES-AG';
   public static GERENTE_REGIONAL = 'GER-REGION';
+  public static PRINCIPAL = 'OPE-OFPPAL';
   // public static INCREMENTO = 'I';
   // public static RETIRO = 'R';
 };
---
+++ src/app/pages/estadistica/principal/chart-widget/principal-vert-chart-widget.component.ts
@@ -0,0 +1,231 @@
+import { formatNumber } from '@angular/common';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import exportData from 'highcharts/modules/export-data';
+import exporting from 'highcharts/modules/exporting';
+import { Observable } from 'rxjs';
+import { Moneda } from 'src/@sirio/domain/services/configuracion/divisa/moneda.service';
+import { SaldoAgenciaReportService } from 'src/@sirio/domain/services/control-efectivo/saldo-agencia.report.service';
+import { ChartBaseComponent } from 'src/@sirio/shared/base/chart-base.component';
+@Component({
+
+  selector: 'sirio-principal-vert-chart-widget',
+  templateUrl: './principal-vert-chart-widget.component.html',
+  styleUrls: ['./principal-vert-chart-widget.component.scss']
+})
+export class PrincipalVertChartWidgetComponent extends ChartBaseComponent implements OnInit {
+
+  @Input() data: Observable<any>;
+  @Input() title: string = 'Estadísticas';
+  @Input() monedas: Observable<Moneda[]>;
+  @Input() moneda_curr: Moneda = undefined;
+  currentMoneda: Moneda;
+  availableCoins: Moneda[] = [];
+  @Input() options: any;
+
+  @Output('reload') refresh: EventEmitter<any> = new EventEmitter<any>();
+
+  highcharts = Highcharts;
+  barChart: any = undefined;
+
+  isLoading: boolean;
+
+  constructor(
+    private agenciaReport: SaldoAgenciaReportService,
+    private cdref: ChangeDetectorRef) {
+    super()
+  }
+  ngOnInit(): void {
+
+    exporting(Highcharts);
+    exportData(this.highcharts);
+    console.log('moneda curr ', this.moneda_curr);
+
+    this.monedas.subscribe(list => {
+      console.log('monedas observable',list);
+
+      this.currentMoneda = this.moneda_curr || list[0];
+      this.availableCoins = list;
+      this.reload();
+    });
+  }
+
+  reportPdf() {
+
+    this.loadingDataForm.next(true);
+    // this.agenciaReport.reportResumen().subscribe(data => {
+    //   this.loadingDataForm.next(false);
+    //   console.log('response:', data);
+    //   const name = this.getFileName(data);
+    //   let blob: any = new Blob([data.body], { type: 'application/octet-stream' });
+    //   this.download(name, blob);
+    // });
+  }
+
+  private reload() {
+
+    if (!this.data) {
+      return;
+    }
+    this.isLoading = true;
+
+
+    this.data.subscribe(dataset => {
+      this.isLoading = false;
+
+      if (!dataset.series) {
+        return;
+      }
+
+
+      //TODO: MEJORAS ESTO LUEGO
+      // let series = dataset.series.map(s => {
+
+      //   return { color: s.color, data: s.data[this.currentMoneda.id], name: s.name }
+      // })
+
+      let labels = dataset.labels;
+      const montoTotal= dataset.series[2].data.reduce((a, b) => a + b);
+
+      let series = dataset.series.map(s => {
+
+        return { color: s.color, data: s.data[this.currentMoneda.id], name: s.name }
+      })
+
+
+      // console.log('series', series);
+      // console.log('label', labels);
+      
+
+      this.barChart = {
+        series: series,
+        lang:this.lang,
+        chart: {
+          type: 'column',
+        },
+        title: {
+          text: this.currentMoneda.nombre,
+        },
+        xAxis: {
+          categories: labels,
+          title: {
+            text: 'Movimientos'
+          }
+        },
+
+        yAxis: {
+          title: {
+            text: 'Montos'
+          }
+        },
+        dataLabels: {
+          enabled: true,
+        },
+
+        plotOptions: {
+          series: {
+            // borderWidth: 0,
+            dataLabels: {
+              enabled: true,
+              formatter: function () {
+                return this.point.y ? formatNumber(this.point.y, 'es', '1.2') : '';
+              }
+              // '{point.y:.1f}'
+            }
+          }
+        },
+      } as Highcharts.ChartOptions;
+
+      /*
+       chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Efficiency Optimization by Branch'
+    },
+    xAxis: {
+        categories: [
+            'Seattle HQ',
+            'San Francisco',
+            'Tokyo'
+        ]
+    },
+    yAxis: [{
+        min: 0,
+        title: {
+            text: 'Employees'
+        }
+    }, {
+        title: {
+            text: 'Profit (millions)'
+        },
+        opposite: true
+    }],
+    legend: {
+        shadow: false
+    },
+    tooltip: {
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            grouping: false,
+            shadow: false,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Employees',
+        color: 'rgba(165,170,217,1)',
+        data: [150, 73, 20],
+        pointPadding: 0.3,
+        pointPlacement: -0.2
+    }, {
+        name: 'Employees Optimized',
+        color: 'rgba(126,86,134,.9)',
+        data: [140, 90, 40],
+        pointPadding: 0.4,
+        pointPlacement: -0.2
+    }, {
+        name: 'Profit',
+        color: 'rgba(248,161,63,1)',
+        data: [183.6, 178.8, 198.5],
+        tooltip: {
+            valuePrefix: '$',
+            valueSuffix: ' M'
+        },
+        pointPadding: 0.3,
+        pointPlacement: 0.2,
+        yAxis: 1
+    }, {
+        name: 'Profit Optimized',
+        color: 'rgba(186,60,61,.9)',
+        data: [203.6, 198.8, 208.5],
+        tooltip: {
+            valuePrefix: '$',
+            valueSuffix: ' M'
+        },
+        pointPadding: 0.4,
+        pointPlacement: 0.2,
+        yAxis: 1
+    }]
+      */
+
+
+
+    })
+
+
+  }
+
+  refreshData() {
+    this.isLoading = true;
+    this.refresh.emit(true);
+  }
+
+  changeMoneda(val) {
+    this.barChart = undefined;
+    this.currentMoneda = val;
+    this.reload();
+  }
+}
