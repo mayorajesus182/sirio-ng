86ddc2f8d17678a71a412cb9ade5b234ce8fe6d7
Karen Rivas <krivas@gmail.com>
2023-01-26 15:31:31 -0400

    reporte agencia con excedente y deficit

---
+++ src/app/pages/control-efectivo/reports/agencia-excedente/form/reporte-agencia-excedente-form.component.ts
@@ -0,0 +1,75 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Injector, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { fadeInRightAnimation } from 'src/@sirio/animations/fade-in-right.animation';
+import { fadeInUpAnimation } from 'src/@sirio/animations/fade-in-up.animation';
+import { RolConstants } from 'src/@sirio/constants';
+import { Region, RegionService } from 'src/@sirio/domain/services/configuracion/gestion-efectivo/region.service';
+import { GestionEfectivoReports, GestionEfectivoReportsService } from 'src/@sirio/domain/services/configuracion/gestion-efectivo/reports/gestion-efectivo-reports.service';
+import { User } from 'src/@sirio/domain/services/security/auth.service';
+import { SessionService } from 'src/@sirio/services/session.service';
+import { FormBaseComponent } from 'src/@sirio/shared/base/form-base.component';
+
+@Component({
+    selector: 'app-reporte-agencia-operativa-form',
+    templateUrl: './reporte-agencia-operativa-form.component.html',
+    styleUrls: ['./reporte-agencia-operativa-form.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [fadeInUpAnimation, fadeInRightAnimation]
+})
+
+export class ReporteAgenciaExcedenteFormComponent extends FormBaseComponent implements OnInit {
+    esGerente: Boolean = false;
+    // esGerenteGeneral: Boolean = false;
+    public regiones = new BehaviorSubject<Region[]>([]);
+    gestionEfectivoReports: GestionEfectivoReports = {} as GestionEfectivoReports;
+    constructor(
+        injector: Injector,
+        dialog: MatDialog,
+        private fb: FormBuilder,
+        private route: ActivatedRoute,
+        private gestionEfectivoReportsService: GestionEfectivoReportsService,
+        private sessionService: SessionService,
+        private regionService: RegionService,
+        private cdr: ChangeDetectorRef) {
+        super(undefined, injector);
+    }
+
+    ngOnInit() {
+        this.buildForm();
+        const user = this.sessionService.getUser() as User;
+        this.esGerente = user.rols.includes(RolConstants.GERENTE_REGIONAL); 
+        
+        this.regionService.actives().subscribe(data => {
+            this.regiones.next(data);
+        });
+    }
+
+    buildForm() {
+        this.itemForm = this.fb.group({
+            region: new FormControl(undefined),
+        });
+    }
+
+    generate() {
+        if (this.itemForm.invalid)
+            return;
+        this.updateData(this.gestionEfectivoReports);
+        this.loadingDataForm.next(true);
+        this.gestionEfectivoReportsService.agenciaOperativa(this.gestionEfectivoReports).subscribe(data => {
+            this.loadingDataForm.next(false);
+            const name = this.getFileName(data);
+            let blob: any = new Blob([data.body], { type: 'application/octet-stream' });
+            this.download(name, blob);
+        });
+        this.itemForm.reset({});
+    }
+}
+
+
+
+
+
+ 
